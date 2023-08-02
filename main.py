3e5aa511f352d090545ecc6480478da0303cd4f8
satyamdas03 <116027621+satyamdas03@users.noreply.github.com>
2023-08-02 17:21:58 +0530

    ggf

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from tkinter import *;
 compiler = Tk()
 compiler.title("myVSC")
+
 menu_bar = Menu(compiler)
 run_bar = Menu(menu_bar)
+run_bar.add_command(label="RUN")
+menu_bar.add_cascade(label="RUN", menu=run_bar)
+
+
+
 editor = Text()
 editor.pack()
 compiler.mainloop()
