57c26818f941dce65edb23924b54a9b93d12cc45
satyamdas03 <116027621+satyamdas03@users.noreply.github.com>
2023-08-02 17:24:31 +0530

    rr

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ from tkinter import *;
 compiler = Tk()
 compiler.title("myVSC")
 
+def run():
+    print("code will get executed")
+
 menu_bar = Menu(compiler)
-run_bar = Menu(menu_bar)
+run_bar = Menu(menu_bar, tearoff=0)
 run_bar.add_command(label="RUN")
 menu_bar.add_cascade(label="RUN", menu=run_bar)
 compiler.config(menu=menu_bar)
