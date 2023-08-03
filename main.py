25cae3701f3d74df83f0c92b3f805460496121dd
satyamdas03 <116027621+satyamdas03@users.noreply.github.com>
2023-08-03 15:13:02 +0530

    ss

--- main.py
+++ main.py
@@ -8,9 +8,16 @@ def run():
     exec(code)
 
 menu_bar = Menu(compiler)
+
+file_menu = Menu(menu_bar, tearoff=0)
+file_menu.add_command(label="Open", command=run)
+menu_bar.add_cascade(label="File", menu=file_menu)
+
 run_bar = Menu(menu_bar, tearoff=0)
 run_bar.add_command(label="RUN", command=run)
 menu_bar.add_cascade(label="RUN", menu=run_bar)
+
+
 compiler.config(menu=menu_bar)
 
 
