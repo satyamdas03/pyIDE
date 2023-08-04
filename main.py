bc89b8ddbb44b9b7bf99bbd0e27c16f6c2348959
satyamdas03 <116027621+satyamdas03@users.noreply.github.com>
2023-08-04 10:21:19 +0530

    ww

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ from tkinter import *;
 from tkinter.filedialog import asksaveasfilename, askopenfilename
 compiler = Tk()
 compiler.title("myVSC")
+file_path = ''
+
+def set_file_path(path):
+    global file_path
+    file_path = path
 
 def open_file():
     path = askopenfilename(filetypes=[('Python Files', '*.py')])
@@ -9,12 +14,17 @@ def open_file():
         code = file.read()
         editor.delete('1.0', END)
         editor.insert('1.0', code)
+        set_file_path(path)
 
 def save_as():
-    path = asksaveasfilename(filetypes=[('Python Files', '*.py')])
+    if file_path == '':
+        path = asksaveasfilename(filetypes=[('Python Files', '*.py')])
+    else:
+        path = file_path
     with open(path, 'w') as file:
         code = editor.get('1.0', END)
         file.write(code)
+        set_file_path(path)
 def run():
     code = editor.get('1.0', END)
     exec(code)
@@ -23,7 +33,7 @@ menu_bar = Menu(compiler)
 
 file_menu = Menu(menu_bar, tearoff=0)
 file_menu.add_command(label="Open", command=open_file)
-file_menu.add_command(label="Save", command=run)
+file_menu.add_command(label="Save", command=save_as)
 file_menu.add_command(label="Save As", command=save_as)
 file_menu.add_command(label="Exit", command=exit)
 menu_bar.add_cascade(label="File", menu=file_menu)
